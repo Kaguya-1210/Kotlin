e7202a920f6ff33f0210ed51d8656615dfc589f7
Kaguya <rider0@163.com>
2024-10-10 09:05:49 +0800

    我勒个韶钢啊，好唐的写法

--- kotlin_project/src/main/kotlin/day01/Control flow.kt
+++ kotlin_project/src/main/kotlin/day01/Control flow.kt
@@ -46,21 +46,11 @@ fun main() {
      */
     var pizzaSlices = 0
     // Start refactoring here
-    pizzaSlices++
-    println("There's only $pizzaSlices slice/s of pizza :(")
-    pizzaSlices++
-    println("There's only $pizzaSlices slice/s of pizza :(")
-    pizzaSlices++
-    println("There's only $pizzaSlices slice/s of pizza :(")
-    pizzaSlices++
-    println("There's only $pizzaSlices slice/s of pizza :(")
-    pizzaSlices++
-    println("There's only $pizzaSlices slice/s of pizza :(")
-    pizzaSlices++
-    println("There's only $pizzaSlices slice/s of pizza :(")
-    pizzaSlices++
-    println("There's only $pizzaSlices slice/s of pizza :(")
-    pizzaSlices++
+    //while写法
+    while (pizzaSlices < 8) {
+        pizzaSlices++
+        println("There's only $pizzaSlices slice/s of pizza :(")
+    }
     // End refactoring here
     println("There are $pizzaSlices slices of pizza. Hooray! We have a whole pizza! :D")
 

54c7c8291c989102b4705a810b1feca52785ac03
Kaguya <rider0@163.com>
2024-10-10 09:07:28 +0800

    简简单单，顺便帮我复习了java

--- kotlin_project/src/main/kotlin/day01/Control flow.kt
+++ kotlin_project/src/main/kotlin/day01/Control flow.kt
@@ -47,10 +47,14 @@ fun main() {
     var pizzaSlices = 0
     // Start refactoring here
     //while写法
-    while (pizzaSlices < 8) {
+//    while (pizzaSlices < 8) {
+//        pizzaSlices++
+//        println("There's only $pizzaSlices slice/s of pizza :(")
+//    }
+    do {
         pizzaSlices++
         println("There's only $pizzaSlices slice/s of pizza :(")
-    }
+    } while (pizzaSlices < 8)
     // End refactoring here
     println("There are $pizzaSlices slices of pizza. Hooray! We have a whole pizza! :D")
 
